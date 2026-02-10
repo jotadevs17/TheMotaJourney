0106d2fb0e589a385a4ec345f493214244762264
mr-robot <joao_flap@hotmail.com>
2026-02-09 22:46:26 -0300

    Feat: Implementa movimentação básica e sprites do player

--- src/main/java/com/jotadevs/main/GamePanel.java
+++ src/main/java/com/jotadevs/main/GamePanel.java
@@ -1,5 +1,7 @@
 package com.jotadevs.main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,7 +12,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = maxScreenCol * tileSize; // 768 pixels
@@ -21,6 +23,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     int playerX = 100;
     int playerY = 100;
@@ -65,27 +68,17 @@ public class GamePanel extends JPanel implements Runnable{
                 delta--;
                 drawCount++;
             }
-        }
 
-        if(timer >= 1000000000) {
-            System.out.println("FPS: " + drawCount);
-            drawCount = 0;
-            timer = 0;
+            if(timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
         }
     }
 
     public void update() {
-
-        if(keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        } else if(keyH.downPressed == true) {
-            playerY += playerSpeed;
-        } else if(keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        } else if(keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
-
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -93,11 +86,7 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-
-        g2.setColor(Color.WHITE);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
-
+        player.draw(g2);
         g2.dispose();
     }
 }
